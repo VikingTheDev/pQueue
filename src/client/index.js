d5cff9f83b0052c13120b65fed4a904bb93b1850
devpetrikov <64926058+devpetrikov@users.noreply.github.com>
2021-01-14 23:29:38 +0100

    Comment added

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 let clientID = setInterval(function () {
-    if (NetworkIsSessionStarted()) {
+    if (NetworkIsSessionStarted()) { // When the user has loaded in to the server, shift the queue
         emitNet('pQueue:shiftQueue');
         clearInterval(clientID);
     }
